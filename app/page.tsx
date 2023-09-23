30926d56a7bd9b6b5622638cf4103f5346ea8b39
Sandhya <thaku.sandhya@gmail.com>
2023-09-23 17:06:41 +0530

    Updated React code

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,87 @@
-import Image from 'next/image'
-import {ModeToggle} from '@/components/mode-toggle'
-import {MenubarDemo} from '@/components/menu-bar'
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+  async function createIndexAndEmbeddings() {
+    try {
+      const result = await fetch("/api/setup", {
+        method: "POST",
+      });
+      const json = await result.json();
+      console.log("result: ", json);
+    } catch (err) {
+      console.log("err:", err);
+    }
+  }
+  async function sendQuery() {
+    if (!query) return;
+    setResult("");
+    setLoading(true);
+    try {
+      const result = await fetch("/api/read", {
+        method: "POST",
+        body: JSON.stringify(query),
+      });
+      const json = await result.json();
+      setResult(json.data);
+      setLoading(false);
+    } catch (err) {
+      console.log("err:", err);
+      setLoading(false);
+    }
+  }
+  const handleFileUpload = async (event) => {
+    const selectedFile = event.target.files[0]; // Get the selected file
+
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append("file", selectedFile); // Append the file to the FormData object
+
+      try {
+        const response = await fetch("/api/upload", {
+          method: "POST",
+          body: formData,
+        });
+
+        if (response.ok) {
+          // File upload was successful
+          console.log("File uploaded successfully.");
+          // You can handle the server's response here
+        } else {
+          // File upload failed
+          console.error("File upload failed.");
+        }
+      } catch (error) {
+        console.error("Error uploading file:", error);
+      }
+    }
+  };
+
   return (
-    <main >
-      
-      <div className="absolute bottom-5 right-5 h-10 w-10 ...">
-      <ModeToggle/>
-      </div>
-      <div>
-        
-      </div>
-      
+    <main className="flex flex-col items-center justify-between p-24">
+      <input
+        className="text-black px-2 py-1"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button
+        className="px-7 py-1 rounded-2xl bg-white text-black mt-2 mb-2"
+        onClick={sendQuery}
+      >
+        Ask AI
+      </button>
+      {loading && <p>Asking AI ...</p>}
+      {result && <p>{result}</p>}
+      {/* consider removing this button from the UI once the embeddings are created ... */}
+
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+        onClick={createIndexAndEmbeddings}
+      >
+        Create index and embeddings
+      </button>
     </main>
-  )
+  );
 }
--- utils.ts
+++ utils.ts
@@ -99,3 +99,48 @@ export const updatePinecone = async (client, indexName, docs) => {
     console.log(`Pinecone index updated with ${chunks.length} vectors`);
   }
 };
+export const queryPineconeVectorStoreAndQueryLLM = async (
+  client,
+  indexName,
+  question
+) => {
+  // 1. Start query process
+  console.log("Querying Pinecone vector store...");
+  // 2. Retrieve the Pinecone index
+  const index = client.Index(indexName);
+  // 3. Create query embedding
+  const queryEmbedding = await new OpenAIEmbeddings().embedQuery(question);
+  // 4. Query Pinecone index and return top 10 matches
+  let queryResponse = await index.query({
+    queryRequest: {
+      topK: 10,
+      vector: queryEmbedding,
+      includeMetadata: true,
+      includeValues: true,
+    },
+  });
+  // 5. Log the number of matches
+  console.log(`Found ${queryResponse.matches.length} matches...`);
+  // 6. Log the question being asked
+  console.log(`Asking question: ${question}...`);
+  if (queryResponse.matches.length) {
+    // 7. Create an OpenAI instance and load the QAStuffChain
+    const llm = new OpenAI({});
+    const chain = loadQAStuffChain(llm);
+    // 8. Extract and concatenate page content from matched documents
+    const concatenatedPageContent = queryResponse.matches
+      .map((match) => match.metadata.pageContent)
+      .join(" ");
+    // 9. Execute the chain with input documents and question
+    const result = await chain.call({
+      input_documents: [new Document({ pageContent: concatenatedPageContent })],
+      question: question,
+    });
+    // 10. Log the answer
+    console.log(`Answer: ${result.text}`);
+    return result.text;
+  } else {
+    // 11. Log that there are no matches, so GPT-3 will not be queried
+    console.log("Since there are no matches, GPT-3 will not be queried.");
+  }
+};
